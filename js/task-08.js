66feea44ae9e821db8fc323f90d26c54fb86d731
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-01-18 22:25:52 +0200

    js-tasks 01-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,21 @@
 // Напиши скрипт управління формою логіна.
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  console.log(`Email: ${email.value}
+Password: ${password.value}`);
+  e.currentTarget.reset();
+};
 
 
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+
+});
